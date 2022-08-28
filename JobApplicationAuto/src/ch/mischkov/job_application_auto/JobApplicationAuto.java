2de9c13cfbf7aee839136d7920bb344a4e885980
Mischkov <Mischkov@PC0.home>
2022-08-28 22:42:44 +0200

    Initial commit

--- JobApplicationAuto/src/ch/mischkov/job_application_auto/JobApplicationAuto.java
+++ JobApplicationAuto/src/ch/mischkov/job_application_auto/JobApplicationAuto.java
@@ -13,7 +13,7 @@ public class JobApplicationAuto {
     * @param args
     */
    public static void main(String[] args) {
-      System.out.println("Hello");
+      System.out.println("Hello") ;
 
    }
 
